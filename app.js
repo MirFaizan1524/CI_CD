42ae124e0317ae5d2d37a8d7aba81eaf142c52e8
faizan ahmad mir <faizan.mir@acmo.in>
2025-07-16 15:13:34 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express  = require("express");
+const app = express();
+require("dotenv").config();
+
+const port = process.env.PORT_NUMBER || 3000;
+
+// using middlewares to parseJson:
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+// possible routes:
+app.get('/',(req,res)=>{
+    console.log("Get route was hit");
+    res.json({success:true,message:"Welcome to the CI CD pipleines"});
+})
+
+
+app.listen(port,()=>{
+   console.log(`listening to the portNumber ${port}`);
+})
+
